741525648bb095bcecaa5d1f9594a0f9ed28cd4b
William <Wintan@student.unimelb.edu.au>
2021-08-23 08:45:22 +1000

    add JWT required for API gateway

--- stacks/sample_sheet_check_stack.py
+++ stacks/sample_sheet_check_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (core as cdk,
                      aws_apigateway as apigateway,
-                     aws_lambda as lambda_)
+                     aws_lambda as lambda_,
+                     aws_cognito as cognito)
 
 class SampleSheetCheckStack(cdk.Stack):
 
@@ -46,6 +47,21 @@ class SampleSheetCheckStack(cdk.Stack):
 
         # Integrate the apigateway with the lambda function
         sampleSheetValidationIntegration = apigateway.LambdaIntegration(sample_sheet_check_lambda)
+        
+        # Pool Config
+        user_pool = cognito.UserPool.from_user_pool_arn(self, "existingUserPool",
+            user_pool_arn="arn:aws:cognito-idp:ap-southeast-2:702956374523:userpool/ap-southeast-2_E5XfPxzX2"
+        )
+
+        # Authorizer config
+        auth_config = apigateway.CognitoUserPoolsAuthorizer(self, "cognitoAuthorizer",
+            cognito_user_pools=[user_pool],
+            authorizer_name = "cognitoAuthorizer",
+            identity_source = apigateway.IdentitySource.header('Authorization')
+        )
 
         # add method of the function for the api gateway
-        api.root.add_method("POST", sampleSheetValidationIntegration)
+        api.root.add_method("POST", sampleSheetValidationIntegration,
+            authorization_type=apigateway.AuthorizationType.COGNITO,
+            authorizer=auth_config
+        )
