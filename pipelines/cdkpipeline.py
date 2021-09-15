8571e9fb812377766b3cce462090be586cbd5f14
William <Wintan@student.unimelb.edu.au>
2021-09-15 22:32:50 +1000

    Implement cdkPipeline

---
+++ pipelines/cdkpipeline.py
@@ -0,0 +1,91 @@
+# importing modules
+from aws_cdk import (
+    aws_ssm as ssm,
+    pipelines,
+    core as cdk,
+    aws_codepipeline as codepipeline,
+    aws_s3 as s3,
+    aws_codepipeline_actions as codepipeline_actions,
+    aws_iam as iam
+)
+from stacks.sscheck_backend_stack import SampleSheetCheckBackEndStack
+
+class SampleSheetCheckBackEndStage(cdk.Stage):
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Create stack defined on stacks folder
+        SampleSheetCheckBackEndStack(self, "SampleSheetCheckEnd")
+
+# Class for the CDK pipeline stack
+class CdkPipelineStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Load SSM parameter for GitHub repo (Created via Console)
+        codestar_arn = ssm.StringParameter.from_string_parameter_attributes(self, "codestarArn",
+            parameter_name="/sscheck/codestar-connection-arn"
+        ).string_value
+
+        cloud_artifact = codepipeline.Artifact(
+            artifact_name="sscheck_pipeline_cloud"
+        )
+
+        source_artifact = codepipeline.Artifact(
+            artifact_name="sscheck_pipeline_source",
+        )
+
+        # Fetch github repository for changes
+        code_star_action = codepipeline_actions.CodeStarConnectionsSourceAction(
+            connection_arn = codestar_arn,
+            output = source_artifact,
+            owner = "umccr",
+            repo = "samplesheet-check-backend",
+            branch = "implement-pipeline",
+            action_name = "Source"
+        )
+
+
+        # Create CDK pipeline
+        pipeline = pipelines.CdkPipeline(
+            self,
+            "CDKPipeline",
+            cloud_assembly_artifact = cloud_artifact,
+            pipeline_name="CDKSampleSheetCheckBackEnd",
+            source_action = code_star_action,
+            synth_action = pipelines.SimpleSynthAction(
+                synth_command = "cdk synth",
+                cloud_assembly_artifact = cloud_artifact,
+                source_artifact = source_artifact,
+                install_commands = [
+                    "npm install -g aws-cdk",
+                    "gem install cfn-nag",
+                    "pip install -r requirements.txt",
+                    "docker -v"
+                ],
+                build_commands = [
+                    "for dir in $(find ./lambdas/layers/ -maxdepth 1 -mindepth 1 -type d); do ./build_lambda_layers.sh ${dir}; done"
+                ],
+                test_commands = [
+                    "mkdir ./cfnnag_output",
+                    "for template in $(find ./cdk.out -type f -maxdepth 2 -name '*.template.json'); do cp $template ./cfnnag_output; done",
+                    "cfn_nag_scan --input-path ./cfnnag_output"
+                ],
+                action_name = "Synth",
+                project_name = "cdk_synth",
+                subdirectory = "deploy"
+            )
+
+        )
+            
+
+        # Deploy infrastructure
+        pipeline.add_application_stage(
+            SampleSheetCheckBackEndStage(
+                self,
+                "SampleSheetCheckBackEndStage",
+            )
+        )
+
+
