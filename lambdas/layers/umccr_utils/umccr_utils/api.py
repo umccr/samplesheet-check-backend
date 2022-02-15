7c10af02420c6d19d82a9178cf1910cfa16b7e25
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-02-15 15:12:38 +1100

    Limit 300 libraries per batch API Call

--- lambdas/layers/umccr_utils/umccr_utils/api.py
+++ lambdas/layers/umccr_utils/umccr_utils/api.py
@@ -17,32 +17,45 @@ async def get_metadata_record_from_array_of_field_name(auth_header: str, path: s
 
     # Removing any duplicates for api efficiency
     value_list = list(set(value_list))
-    
-    # Define query string
-    query_param_string = f'&{field_name}='.join(value_list)
-    query_param_string = f'?{field_name}=' + query_param_string  # Appending name at the beginning
 
-    query_param_string = query_param_string + f'&rowsPerPage=1000'  # Add Rows per page (1000 is the maximum rows)
-
-    url = "https://" + DATA_PORTAL_DOMAIN_NAME + "/" + path.strip('/') + query_param_string
-
+    # Result variable
     query_result = []
-    print('Start API request')
+
     async with ClientSession() as session:
 
-        # Make sure no data is left, looping data until the end
-        while url is not None:
+        # Set maximum batch API call to 300 to prevent "Request Header Fields Too Large"
+        max_number_of_library_per_api_call = 300
+        for i in range(0, len(value_list), max_number_of_library_per_api_call):
 
-            async with session.get(url, headers=headers) as response:
+            # Define start and stop element from the list
+            start_index = i
+            end_index = start_index + max_number_of_library_per_api_call
 
-                # API call
-                response_json = await response.json()
+            array_to_process = value_list[start_index:end_index]
 
-                # Raise an error for non 200 status code
-                if response.status < 200 or response.status >= 300:
-                    raise ValueError(f'Non 20X status code returned')
+            print(len(array_to_process))
 
-                query_result.extend(response_json["results"])
-                url = response_json["links"]["next"]
-    print('Finish API request')
+            # Define query string
+            query_param_string = f'&{field_name}='.join(array_to_process)
+            query_param_string = f'?{field_name}=' + query_param_string  # Appending name at the beginning
+
+            query_param_string = query_param_string + f'&rowsPerPage=1000'  # Add Rows per page (1000 is the maximum rows)
+
+            url = "https://" + DATA_PORTAL_DOMAIN_NAME + "/" + path.strip('/') + query_param_string
+
+            # Make sure no data is left, looping data until the end
+            while url is not None:
+
+                async with session.get(url, headers=headers) as response:
+
+                    # API call
+                    response_json = await response.json()
+
+                    # Raise an error for non 200 status code
+                    if response.status < 200 or response.status >= 300:
+                        raise ValueError(f'Non 20X status code returned')
+
+                    query_result.extend(response_json["results"])
+                    url = response_json["links"]["next"]
+
     return query_result
