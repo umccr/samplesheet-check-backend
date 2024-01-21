b0c70bf0608e58bd325a36112e97dd6815d7719a
alexiswl <8197659+alexiswl@users.noreply.github.com>
2024-01-22 10:24:18 +1100

    Add cloud components to V2 samplesheet
    Adds hardcoded URN to BCLConvert settings, adds default cloud settings section.  
    
    Update v2 samplesheet maker project version

--- src/samplesheet/v2_samplesheet_builder.py
+++ src/samplesheet/v2_samplesheet_builder.py
@@ -17,8 +17,11 @@ import pandas as pd
 from utils import camel_to_snake
 from utils.samplesheet import SampleSheet, check_global_override_cycles
 from utils.logger import get_logger
-from utils.globals import V2_SAMPLESHEET_BCLCONVERT_ADAPTER_SETTINGS_BY_ASSAY_TYPE, \
-    V2_ADAPTER_SETTINGS, V2_DATA_ROWS, V2_SAMPLESHEET_GLOBAL_SETTINGS, V2_SAMPLESHEET_DATA_SETTINGS
+from utils.globals import (
+    V2_SAMPLESHEET_BCLCONVERT_ADAPTER_SETTINGS_BY_ASSAY_TYPE,
+    V2_ADAPTER_SETTINGS, V2_DATA_ROWS, V2_SAMPLESHEET_GLOBAL_SETTINGS, V2_SAMPLESHEET_DATA_SETTINGS,
+    V2_BCLCONVERT_BASESPACE_URN
+)
 from v2_samplesheet_maker.classes.samplesheet import SampleSheet as SampleSheetV2
 
 logger = get_logger()
@@ -223,7 +226,10 @@ def get_bclconvert_settings_dict(samplesheet: SampleSheet) -> Dict:
     for setting_key, setting_value in bclconvert_settings_dict.items():
         bclconvert_settings_dict[setting_key] = V2_SAMPLESHEET_GLOBAL_SETTINGS[setting_key](setting_value)
 
-    # Return bclconvert settings dcit
+    # Add in BCLConvert URN
+    bclconvert_settings_dict["urn"] = V2_BCLCONVERT_BASESPACE_URN
+
+    # Return bclconvert settings dict
     return bclconvert_settings_dict
 
 
@@ -404,12 +410,18 @@ def v1_samplesheet_to_json(samplesheet: SampleSheet) -> Dict:
         bclconvert_data_list
     )
 
+    cloud_settings_section = {
+        "generated_version": "0.0.0",
+        "cloud_workflow": "ica_workflow_1"
+    }
+
     # Write out json
     return {
         "header": header_dict,
         "reads": reads_dict,
         "bclconvert_settings": bclconvert_settings_dict,
-        "bclconvert_data": bclconvert_data_list
+        "bclconvert_data": bclconvert_data_list,
+        "cloud_settings": cloud_settings_section
     }
 
 
